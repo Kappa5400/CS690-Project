fc5587edd79048595de168b78e39a5f2b93c4317
Kappa54 <colinburdett54@gmail.com>
2026-05-21 20:14:10 +0000

    wip

--- Garden/InitDB.cs
+++ Garden/InitDB.cs
@@ -156,6 +156,36 @@ class InitDB{
 
     }
 
+    static void getAllGarderner(int index)
+    {
+        using var connection = newSqliteConnection(ConnectionString);
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM garderner";
+
+        using var reader = command.ExcecuteReader();
+
+        while (reader.Read())
+        {
+            long id = reader.GetInt64(0);
+            bool toolUsing = reader.GetBool(1);
+            bool plotOwn = reader.GetBool(2);
+            int toolUsingIndex = reader.GetInt64(3);
+            int plotOwnIndex = reader.GetInt64(4);
+            string name = reader.GetString(5);
+            Console.WriteLine($@"ID: {id}
+            Name: {name}
+            Using a tool currently: {toolUsing}
+            Owns a plot: {plotOwn}
+            Index of tool if using: {toolUsingIndex}
+            Index of plot if own: {plotOwnIndex}
+            \n
+            ");
+        }
+
+    }
+
     static void getVolunteerViaIndex(int index)
     {
         using var connection = newSqliteConnection(ConnectionString);
