a01feb4058bc5fee553185c8c1974cbea019c171
Kappa54 <colinburdett54@gmail.com>
2026-05-21 19:45:58 +0000

    sql in progress

---
+++ Garden/HelperDB.cs
@@ -0,0 +1,7 @@
+namespace Garden;
+
+using Microsoft.Data.Sqlite;
+using System;
+
+
+
--- Garden/InitDB.cs
+++ Garden/InitDB.cs
@@ -14,7 +14,12 @@ class InitDB{
     static void init()
     {
         InitializeDatabase();
-
+        initGardernerTable();
+        initVolunteerTable();
+        initSudoTable();
+        initTaskTable();
+        initToolTable();
+        initPlotTable();
     }
 
     static void InitializeDatabase()
@@ -115,6 +120,81 @@ class InitDB{
         createTaskTable.ExcecuteNonQuery();
     }
 
+    //below are helper functions
+
+    //View functions
+
+    static void getGardernerViaIndex(int index)
+    {
+        using var connection = newSqliteConnection(ConnectionString);
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM garderner WHERE gardernerIndex = $index";
+        command.Parameters.AddWithValue("$index", index);
+
+        using var reader = command.ExcecuteReader();
+
+        while (reader.Read())
+        {
+            long id = reader.GetInt64(0);
+            int task = reader.GetInt64(1);
+            string name = reader.GetString(2);
+        }
+
+        /*
+        CREATE TABLE IF NOT EXISTS volunteer (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            task INTEGER,
+            name STRING
+        */
+
+        Console.WriteLine($@"ID: {id}
+            Name: {name}
+            Assigned task: {task}
+            ");
+
+    }
+
+    static void getVolunteerViaIndex(int index)
+    {
+        using var connection = newSqliteConnection(ConnectionString);
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM volunteere WHERE volunteerIndex = $index";
+        command.Parameters.AddWithValue("$index", index);
+
+        using var reader = command.ExcecuteReader();
+
+        while (reader.Read())
+        {
+            long id = reader.GetInt64(0);
+            bool toolUsing = reader.GetBool(1);
+            bool plotOwn = reader.GetBool(2);
+            int toolUsingIndex = reader.GetInt64(3);
+            int plotOwnIndex = reader.GetInt64(4);
+            string name = reader.GetString(5);
+        }
+
+        /*
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            toolUsing BOOL DEFAULT 0,
+            plotOwn BOOL DEFAULT 0,
+            toolUsingIndex INTEGER,
+            plotOwnIndex INTEGER,
+            name STRING
+        */
+
+        Console.WriteLine($@"ID: {id}
+            Name: {name}
+            Using a tool currently: {toolUsing}
+            Owns a plot: {plotOwn}
+            Index of tool if using: {toolUsingIndex}
+            Index of plot if own: {plotOwnIndex}
+            ");
+
+    }
 
 }
 
